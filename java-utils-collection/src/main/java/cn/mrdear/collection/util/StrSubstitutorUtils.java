20ae6db5e4407f6d17d9c6846cf60af20c488407
mrdear <niudear@foxmail.com>
2020-03-06 06:47:51 +0800

    1. 删除注册中心 2. 增加字符串替换工具

---
+++ java-utils-collection/src/main/java/cn/mrdear/collection/util/StrSubstitutorUtils.java
@@ -0,0 +1,13 @@
+package cn.mrdear.collection.util;
+
+/**
+ * apache common text字符串模板替换工具
+ * {@link org.apache.commons.text.StringSubstitutor} 已经提供了相当好用的方法
+ * @author Quding Ding
+ * @since 2020/3/6
+ */
+public class StrSubstitutorUtils {
+
+
+
+}
